8b95218d3694b2b1f2730bfc332a644df4ba18b4
sergiu <frunza.sergiu3@gmai.com>
2021-03-06 14:58:37 -0500

    running app

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,4 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  user = User.create(email: 'test@mail.ru' , password: '111111', password_confirmation: '111111')
